9259538749c8e81d37398affe37ac7efad93ecf0
Chepman123 <Vladshlapak333@gmail.com>
2025-09-27 12:44:29 +0200

    preview()

--- src/components/Main/Services/Services.tsx
+++ src/components/Main/Services/Services.tsx
@@ -4,7 +4,8 @@ import Header from "../../Header/Header";
 import Service from "./Service/Service";
 import classes from './Services.module.css'
 import type { serviceExample } from '/Dantway/stomatology-site/src/data/services';
-import { servicesData } from '/Dantway/stomatology-site/src/data/services';
+import { servicesData } from "../../../data/services";
+
 
 const serviceTypes: { label: string; type: serviceExample["type"] }[] = [
   { label: "1. Profilaktyka", type: "Profilaktyka" },
