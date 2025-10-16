094cede2613cc5ad3851fb9792843cb32e5c7aad
Chepman123 <Vladshlapak333@gmail.com>
2025-10-16 09:49:46 +0200

    fx

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,4 +13,5 @@ app.use(express.json());
 
 app.use('/',Routes());
 
-app.listen(5000, () => console.log("Server started on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
