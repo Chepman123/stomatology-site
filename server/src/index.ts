b2d3ce47a874cbd171d37fa454854a274497990f
Chepman123 <Vladshlapak333@gmail.com>
2025-10-16 09:58:54 +0200

    fx

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,13 +5,24 @@ import helmet from "helmet";
 
 const app = express();
 
-app.use(cors({ origin: '*' }));
+app.use(cors({
+  origin: 'https://dantway.pl',
+  methods: ['GET','POST','OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true
+}));
 
 app.use(helmet());
 app.use(express.json());
 
+app.options('*', cors({
+  origin: 'https://dantway.pl',
+  methods: ['GET','POST','OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true
+}));
 
-app.use('/',Routes());
+app.use('/', Routes());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
