38575982d0968743ad85287ffce83dfc72d6961d
Chepman123 <Vladshlapak333@gmail.com>
2025-10-16 17:53:03 +0200

    fx

--- server/src/Controllers/Book.ts
+++ server/src/Controllers/Book.ts
@@ -5,5 +5,6 @@ export default class BookController{
     constructor(private serv:BookServ){}
     Book(req:Request,res:Response){
      this.serv.Book(req.body.login,req.body.data,req.body.hour,req.body.service,req.body.login);
+     res.status(200).json({ message: 'Booking created' });
     }
 }
